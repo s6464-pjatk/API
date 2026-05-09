fdd61552adfa2d1d4656d17e22f6bf968bd4cfeb
s6464-pjatk <s6464@pjwstk.edu.pl>
2026-05-09 17:53:29 +0200

    Tworzenie kontrolerów

--- API/Controllers/ReservationsController.cs
+++ API/Controllers/ReservationsController.cs
@@ -1,6 +1,41 @@
-﻿namespace API.Controllers
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
 {
-    public class ReservationsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReservationsController : Controller
     {
+        [HttpGet]
+        public ActionResult<IEnumerable<Reservation>> GetAll(
+        [FromQuery] DateOnly? date,
+        [FromQuery] string? status,
+        [FromQuery] int? roomId)
+        {
+            var reservations = TrainingCenterData.Reservations.AsEnumerable();
+
+            if (date.HasValue)
+            {
+                reservations = reservations.Where(reservation => reservation.Date == date.Value);
+            }
+
+            if (!string.IsNullOrWhiteSpace(status))
+            {
+                reservations = reservations.Where(reservation => string.Equals(
+                    reservation.Status,
+                    status,
+                    StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (roomId.HasValue)
+            {
+                reservations = reservations.Where(reservation => reservation.RoomId == roomId.Value);
+            }
+
+            return Ok(reservations);
+        }
+
     }
 }
